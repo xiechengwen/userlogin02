e501b7bd5e35edb9013bf8b1b3f4aa7da9633259
Alex <1528183367@qq.com>
2022-05-09 08:25:37 +0800

    Initial commit

---
+++ src/main/java/com/naughty/userlogin02/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.naughty.userlogin02.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.naughty.userlogin02.bean.User;
+import com.naughty.userlogin02.dao.UserDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+    @Autowired
+    UserDao userDao;
+
+    @CrossOrigin
+    @RequestMapping("/login")
+    public String userLogin(@RequestBody User user) {
+        System.out.println("User : " + user);
+        String str = "error";
+        int count = userDao.getUserByMassage(user.getUsername(), user.getPassword());
+        if (count > 0) {
+            str = "ok";
+        }
+        return str;
+    }
+}
